f5112b39c3f577ea1a67ed240e883281bcb611a6
cmyui <cmyuiosu@gmail.com>
2024-07-14 03:45:42 -0400

    Add leaderboard banner svg

---
+++ src/components/images/banners/LeaderboardBanner.tsx
@@ -0,0 +1,161 @@
+export const LeaderboardBanner = () => {
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 1700 300"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_139_705)">
+        <rect width="1700" height="300" fill="white" />
+        <rect width="1700" height="300" fill="url(#paint0_linear_139_705)" />
+        <g clip-path="url(#clip1_139_705)">
+          <rect
+            width="1902"
+            height="300"
+            transform="translate(-101)"
+            fill="#111111"
+          />
+          <g opacity="0.25">
+            <g style={{ mixBlendMode: "hard-light" }}>
+              <circle
+                cx="1159.57"
+                cy="-187.429"
+                r="553.571"
+                fill="#6000FE"
+                fill-opacity="0.25"
+              />
+            </g>
+            <g style={{ mixBlendMode: "hard-light" }}>
+              <circle
+                cx="363.571"
+                cy="-253.429"
+                r="553.571"
+                fill="#0047FF"
+                fill-opacity="0.25"
+              />
+            </g>
+            <g style={{ mixBlendMode: "hard-light" }}>
+              <circle
+                cx="1198.57"
+                cy="678.571"
+                r="553.571"
+                fill="#4377FE"
+                fill-opacity="0.25"
+              />
+            </g>
+            <g style={{ mixBlendMode: "hard-light" }}>
+              <circle
+                cx="486.571"
+                cy="-161.429"
+                r="553.571"
+                fill="#A143FE"
+                fill-opacity="0.25"
+              />
+            </g>
+          </g>
+        </g>
+        <mask
+          id="mask0_139_705"
+          style={{ maskType: "alpha" }}
+          maskUnits="userSpaceOnUse"
+          x="-190"
+          y="-807"
+          width="1943"
+          height="2040"
+        >
+          <circle
+            cx="1159.57"
+            cy="-187.429"
+            r="552.071"
+            stroke="white"
+            stroke-width="3"
+          />
+          <circle
+            cx="363.571"
+            cy="-253.429"
+            r="552.071"
+            stroke="white"
+            stroke-width="3"
+          />
+          <circle
+            cx="1198.57"
+            cy="678.571"
+            r="552.071"
+            stroke="white"
+            stroke-width="3"
+          />
+          <circle
+            cx="486.571"
+            cy="-161.429"
+            r="552.071"
+            stroke="white"
+            stroke-width="3"
+          />
+        </mask>
+        <g mask="url(#mask0_139_705)">
+          <rect x="-101" width="1902" height="300" fill="#D9D9D9" />
+          <rect
+            x="-101"
+            width="1902"
+            height="300"
+            fill="url(#paint1_linear_139_705)"
+          />
+          <rect
+            x="-101"
+            width="1902"
+            height="300"
+            fill="url(#paint2_linear_139_705)"
+          />
+        </g>
+      </g>
+      <defs>
+        <linearGradient
+          id="paint0_linear_139_705"
+          x1="850"
+          y1="-108"
+          x2="992.58"
+          y2="390.739"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#4377FE" />
+          <stop offset="1" stop-color="#7B2CFB" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_139_705"
+          x1="850"
+          y1="-108"
+          x2="979.404"
+          y2="398.435"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#EF43FE" />
+          <stop offset="1" stop-color="#2C97FB" />
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_139_705"
+          x1="850"
+          y1="-108"
+          x2="979.404"
+          y2="398.435"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#4377FE" />
+          <stop offset="1" stop-color="#7B2CFB" />
+        </linearGradient>
+        <clipPath id="clip0_139_705">
+          <rect width="1700" height="300" fill="white" />
+        </clipPath>
+        <clipPath id="clip1_139_705">
+          <rect
+            width="1902"
+            height="300"
+            fill="white"
+            transform="translate(-101)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
