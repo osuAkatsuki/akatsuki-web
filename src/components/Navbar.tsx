6af533b1a062777b16948b2dca41a40227745851
cmyui <cmyuiosu@gmail.com>
2024-07-14 23:22:04 -0400

    Start reworking navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,6 +10,8 @@ import {
   Autocomplete,
   debounce,
   Box,
+  AppBar,
+  Toolbar,
 } from "@mui/material"
 import { Link, useNavigate } from "react-router-dom"
 import { FavoriteOutlined } from "@mui/icons-material"
@@ -22,6 +24,24 @@ import {
   SingleUserSearchResult,
 } from "../adapters/akatsuki-api/search"
 import { UserPrivileges } from "../privileges"
+
+const NavButton = ({
+  to,
+  label,
+  sx = {},
+}: {
+  to: string
+  label: string
+  sx?: any
+}) => (
+  <Box sx={sx} maxWidth="lg">
+    <Link to={to}>
+      <Button>
+        <Typography variant="subtitle1">{label}</Typography>
+      </Button>
+    </Link>
+  </Box>
+)
 
 export default function Navbar() {
   const navigate = useNavigate()
@@ -70,114 +90,82 @@ export default function Navbar() {
   }, [searchQuery, searchForUsers])
 
   return (
-    <>
-      <Paper elevation={1} square>
-        <Container>
-          <Stack direction="row" justifyContent="space-between" padding={1.25}>
-            {/* Left Navbar */}
-            <Stack
-              direction="row"
-              spacing={1}
-              sx={{ display: "flex", alignItems: "center" }}
-            >
-              <Link to="/">
-                <Button>
-                  <Box width={146.25} height={29.97}>
-                    <AkatsukiLogo />
-                  </Box>
-                </Button>
-              </Link>
-              <Divider orientation="vertical" flexItem />
-              <Link to="/leaderboards">
-                <Button>
-                  <Typography variant="subtitle1">Leaderboards</Typography>
-                </Button>
-              </Link>
-              {/* <Link to="/information">
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          {/* Left Menu */}
+          <Link to="/">
+            <Button>
+              <Box width={146.25} height={29.97}>
+                <AkatsukiLogo />
+              </Box>
+            </Button>
+          </Link>
+          <Divider orientation="vertical" flexItem />
+          <NavButton to="/leaderboards" label="Leaderboards" />
+          {/* <Link to="/information">
                 <Button>
                   <Typography variant="subtitle1">Information</Typography>
                 </Button>
               </Link> */}
-              <Link to={process.env.REACT_APP_PUBLIC_DISCORD_INVITE_URL!}>
-                <Button>
-                  <Typography variant="subtitle1">Discord</Typography>
-                </Button>
+          <NavButton
+            to={process.env.REACT_APP_PUBLIC_DISCORD_INVITE_URL!}
+            label="Discord"
+            sx={{ flexGrow: 1 }}
+          />
+          {/* Right Menu */}
+          {identity !== null &&
+          identity.privileges & UserPrivileges.ADMIN_ACCESS_RAP ? (
+            <NavButton
+              to={process.env.REACT_APP_ADMIN_PANEL_HOME_URL!}
+              label="Admin Panel"
+            />
+          ) : null}
+
+          <Autocomplete
+            id="user-search"
+            sx={{ width: 225 }}
+            filterOptions={(x) => x}
+            value={searchQueryValue}
+            options={searchQueryOptions ?? []}
+            getOptionLabel={(option) => option.username}
+            isOptionEqualToValue={(option, value) =>
+              option.username === value.username
+            }
+            renderInput={(params) => {
+              return <TextField {...params} label="Looking for someone?" />
+            }}
+            onInputChange={(event, newInputValue: string) =>
+              setSearchQuery(newInputValue)
+            }
+            onChange={(event, newValue) => {
+              if (newValue === null) return
+              setSearchQueryValue(newValue)
+              setSearchQueryOptions([newValue])
+              navigate(`/u/${newValue.id}`)
+            }}
+          />
+          {identity !== null ? (
+            <>
+              <Link to="/support">
+                <IconButton aria-label="support">
+                  <FavoriteOutlined sx={{ color: "#db2828" }} />
+                </IconButton>
               </Link>
-            </Stack>
-            {/* Right Navbar */}
-            <Stack
-              direction="row"
-              spacing={1}
-              sx={{ display: "flex", alignItems: "center" }}
-            >
-              {identity !== null &&
-              identity.privileges & UserPrivileges.ADMIN_ACCESS_RAP ? (
-                <Link to={process.env.REACT_APP_ADMIN_PANEL_HOME_URL!}>
-                  <Button>
-                    <Typography variant="subtitle1" fontWeight="bold">
-                      Admin Panel
-                    </Typography>
-                  </Button>
-                </Link>
-              ) : null}
 
-              <Autocomplete
-                id="user-search"
-                sx={{ width: 225 }} // TODO: does this scale?
-                filterOptions={(x) => x}
-                value={searchQueryValue}
-                options={searchQueryOptions ?? []}
-                getOptionLabel={(option) => option.username}
-                isOptionEqualToValue={(option, value) =>
-                  option.username === value.username
-                }
-                renderInput={(params) => {
-                  return <TextField {...params} label="Looking for someone?" />
-                }}
-                onInputChange={(event, newInputValue: string) =>
-                  setSearchQuery(newInputValue)
-                }
-                onChange={(event, newValue) => {
-                  if (newValue === null) return
-                  setSearchQueryValue(newValue)
-                  setSearchQueryOptions([newValue])
-                  navigate(`/u/${newValue.id}`)
-                }}
-              />
-              {identity !== null ? (
-                <>
-                  {/* TODO: dropdown when searching for users */}
-                  {/* https://mui.com/material-ui/react-autocomplete/#search-as-you-type */}
-                  {/* import { debounce } from '@mui/material/utils'; */}
-                  <Link to="/support">
-                    <IconButton aria-label="support">
-                      <FavoriteOutlined sx={{ color: "#db2828" }} />
-                    </IconButton>
-                  </Link>
-
-                  {/* TODO: put this behind a profile submenu */}
-                  <Button onClick={handleLogout}>
-                    <Typography variant="subtitle1">Logout</Typography>
-                  </Button>
-                </>
-              ) : (
-                <>
-                  <Link to="/login">
-                    <Button>
-                      <Typography variant="subtitle1">Login</Typography>
-                    </Button>
-                  </Link>
-                  <Link to="/register">
-                    <Button>
-                      <Typography variant="subtitle1">Register</Typography>
-                    </Button>
-                  </Link>
-                </>
-              )}
-            </Stack>
-          </Stack>
-        </Container>
-      </Paper>
-    </>
+              {/* TODO: put this behind a profile submenu */}
+              <Button onClick={handleLogout}>
+                <Typography variant="subtitle1">Logout</Typography>
+              </Button>
+            </>
+          ) : (
+            <>
+              <NavButton to="/login" label="Login" />
+              <NavButton to="/register" label="Register" />
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
