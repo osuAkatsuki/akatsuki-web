7a7d7d0e850cc1ee04c77bc4e81dcdcb6acb4a1c
cmyui <cmyuiosu@gmail.com>
2024-07-20 18:00:47 -0400

    rewrite navbar again

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,8 +20,7 @@ export default function Footer() {
           position="absolute"
           top={0}
           left={0}
-          zIndex={0}
-          display="block"
+          zIndex={-1}
           width="100%"
           height="100%"
           sx={{
@@ -33,78 +32,73 @@ export default function Footer() {
 
         <Container sx={{ height: "100%" }}>
           <Stack
-            direction="row"
-            position="relative"
+            direction="column"
             justifyContent="space-between"
             height="100%"
-            py={3}
           >
-            {/* Left side footer */}
             <Stack
-              direction="column"
+              direction="row"
               justifyContent="space-between"
-              height="100%"
+              alignItems="center"
+              pt={4}
             >
               <Box width={178} height={38}>
                 <FooterLogo />
               </Box>
-
-              <Stack direction="row" alignItems="center" gap={0.5}>
+              <Stack direction="row" spacing={2} textAlign="right">
+                <Stack direction="column" spacing={2}>
+                  <Typography variant="h6" fontWeight="lighter">
+                    Akatsuki Team
+                  </Typography>
+                  <Typography variant="h6" fontWeight="lighter">
+                    Contact Us
+                  </Typography>
+                </Stack>
+                <Stack direction="column" spacing={2}>
+                  <Typography variant="h6" fontWeight="lighter">
+                    Community Guidelines
+                  </Typography>
+                  <Typography variant="h6" fontWeight="lighter">
+                    Terms of Service
+                  </Typography>
+                </Stack>
+              </Stack>
+            </Stack>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+              pb={4}
+            >
+              <Stack direction="row">
                 <Copyright />
-                <Typography variant="subtitle1" fontWeight="lighter">
+                <Typography variant="h6" fontWeight="lighter">
                   Akatsuki 2024
                 </Typography>
               </Stack>
-            </Stack>
-
-            {/* Right side footer */}
-            <Stack
-              direction="column"
-              justifyContent="space-between"
-              alignItems="flex-end"
-              height="100%"
-            >
-              <Stack direction="row" spacing={5}>
-                <Stack direction="column" spacing={1} textAlign="right">
-                  {/* TODO: link these to pages */}
-                  <Typography fontWeight="lighter">Akatsuki Team</Typography>
-                  <Typography fontWeight="lighter">Contact Us</Typography>
-                </Stack>
-
-                <Stack direction="column" spacing={1} textAlign="right">
-                  {/* TODO: link these to pages */}
-                  <Typography fontWeight="lighter">
-                    Community Guidelines
-                  </Typography>
-                  <Typography fontWeight="lighter">Terms of Service</Typography>
-                </Stack>
-              </Stack>
-
-              <Stack direction="row" spacing={1}>
-                <Link to={process.env.REACT_APP_PUBLIC_DISCORD_INVITE_URL!}>
-                  <Box width={24} height={24}>
+              <Stack direction="row" spacing={2}>
+                <Link to="/discord">
+                  <Box height={36} width={36}>
                     <DiscordLogo />
                   </Box>
                 </Link>
-
                 <Link
-                  to={process.env.REACT_APP_PUBLIC_TWITTER_INVITE_URL!}
+                  to="/github"
                   style={{
                     color: "#FFFFFF",
                     textDecoration: "none",
                   }}
                 >
-                  <Twitter />
+                  <GitHub sx={{ width: 36, height: 36 }} />
                 </Link>
-
                 <Link
-                  to={process.env.REACT_APP_PUBLIC_GITHUB_INVITE_URL!}
+                  to="/twitter"
                   style={{
                     color: "#FFFFFF",
                     textDecoration: "none",
                   }}
                 >
-                  <GitHub />
+                  <Twitter sx={{ width: 36, height: 36 }} />
                 </Link>
               </Stack>
             </Stack>
