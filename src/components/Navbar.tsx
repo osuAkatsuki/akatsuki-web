f8903444301f4fc293a52e380040d7fe7c32484f
cmyui <cmyuiosu@gmail.com>
2024-08-01 19:42:54 +0200

    undo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -106,7 +106,7 @@ export const ProfileSettingsMenu = ({
         >
           <Typography variant="body1">Profile</Typography>
         </MenuItem>
-        {(identity.privileges & UserPrivileges.ADMIN_ACCESS_RAP) !== 0 && (
+        {identity.privileges & UserPrivileges.ADMIN_ACCESS_RAP && (
           <MenuItem
             component={Link}
             onClick={handleClose}
