ed53bbc43b594e90e0e4c33d08ced463690dcc4b
cmyui <cmyuiosu@gmail.com>
2025-06-10 02:59:12 -0400

    maybe working page

--- src/pages/ResetPasswordPage.tsx
+++ src/pages/ResetPasswordPage.tsx
@@ -6,13 +6,17 @@ import {
   Container,
   Divider,
   Stack,
+  TextField,
   Typography,
   useMediaQuery,
   useTheme,
 } from "@mui/material"
+import { useState } from "react"
 import { useSearchParams } from "react-router-dom"
 
+import { verifyPasswordReset } from "../adapters/akatsuki-api/authentication"
 import StaticPageBanner from "../components/images/banners/static_page_banner.svg"
+import { LoginDoorIcon } from "../components/images/icons/LoginDoorIcon"
 
 export const ResetPasswordPage = () => {
   const [queryParams] = useSearchParams()
@@ -21,12 +25,29 @@ export const ResetPasswordPage = () => {
   const theme = useTheme()
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"))
 
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState("")
+
   if (!token) {
     return (
       <Alert severity="error">
         No password reset token provided. Please check the link you received.
       </Alert>
     )
+  }
+
+  const handleSubmit = async () => {
+    try {
+      setLoading(true)
+      await verifyPasswordReset(token, password)
+    } catch (e: any) {
+      setLoading(false)
+      setError(e.message)
+      return
+    }
+    // TODO: auto-auth them or redirect them to the login page
   }
 
   return (
@@ -52,16 +73,98 @@ export const ResetPasswordPage = () => {
           >
             <Stack direction="column" alignItems="center">
               <SettingsIcon />
-              <Typography variant="h5">User Settings</Typography>
+              <Typography variant="h5">Password Reset</Typography>
             </Stack>
           </Box>
           <Box bgcolor="#191527">
             <Stack direction="column" spacing={2} p={2}>
-              <Button></Button>
-              <Divider />
-              <Button></Button>
-              <Divider />
-              <Button></Button>
+              <TextField
+                fullWidth
+                id="password"
+                label="Password"
+                type="password"
+                autoComplete="new-password"
+                InputProps={{
+                  sx: {
+                    borderRadius: 3,
+                    bgcolor: "#110E1B",
+                    borderColor: "red",
+                    mt: 1,
+                  },
+                }}
+                InputLabelProps={{ sx: { mt: 1 } }}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setPassword(e.target.value)
+                }
+                onKeyDown={async (e: React.KeyboardEvent<HTMLInputElement>) => {
+                  if (e.key === "Enter" && password && confirmPassword) {
+                    await handleSubmit()
+                  }
+                  if (e.key === "Tab") {
+                    e?.stopPropagation()
+                  }
+                }}
+              />
+              <TextField
+                fullWidth
+                id="confirm-password"
+                label="Confirm Password"
+                type="password"
+                InputProps={{
+                  sx: {
+                    borderRadius: 3,
+                    bgcolor: "#110E1B",
+                    borderColor: "red",
+                    mt: 1,
+                  },
+                }}
+                InputLabelProps={{ sx: { mt: 1 } }}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setConfirmPassword(e.target.value)
+                }
+                onKeyDown={async (e: React.KeyboardEvent<HTMLInputElement>) => {
+                  if (e.key === "Enter" && password && confirmPassword) {
+                    await handleSubmit()
+                  }
+                  if (e.key === "Tab") {
+                    e?.stopPropagation()
+                  }
+                }}
+              />
+              {error && (
+                <Alert sx={{ mt: 1 }} severity="error">
+                  {error}
+                </Alert>
+              )}
+              <Button
+                fullWidth
+                variant="contained"
+                onClick={handleSubmit}
+                sx={{
+                  backgroundImage:
+                    "linear-gradient(90.09deg, #387EFC -0.08%, #C940FD 99.3%)",
+                  mt: 1.5,
+                  borderRadius: 3,
+                }}
+                onKeyDown={(e: any) => {
+                  if (e.key === "Tab") {
+                    e?.stopPropagation()
+                  }
+                }}
+                disabled={
+                  password === "" ||
+                  confirmPassword === "" ||
+                  password !== confirmPassword ||
+                  loading
+                }
+              >
+                <Stack direction="row" alignItems="center">
+                  <Box width={24} height={24}>
+                    <LoginDoorIcon />
+                  </Box>
+                  <Typography variant="body2">Reset Password</Typography>
+                </Stack>
+              </Button>
             </Stack>
           </Box>
         </Stack>
