b5728e177453fadc853ae407c583b2b67d5308de
cmyui <cmyuiosu@gmail.com>
2025-06-14 03:24:56 -0400

    isReadyForSubmission()

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,6 +30,7 @@ import {
 } from "../adapters/akatsuki-api/search"
 import HomepageBanner from "../components/images/banners/homepage_banner.svg"
 import { Identity, useIdentityContext } from "../context/identity"
+import { validatePassword, validateUsername } from "../security"
 import { LoginDoorIcon } from "./images/icons/LoginDoorIcon"
 import { UserFriendsIcon } from "./images/icons/UserFriendsIcon"
 import { UserLogoutIcon } from "./images/icons/UserLogoutIcon"
@@ -116,6 +117,15 @@ export const AuthenticationSettingsMenu = ({
     setServerError("")
   }
 
+  const isReadyForSubmission = (): boolean => {
+    return !(
+      !validateUsername(username) ||
+      !validatePassword(password) ||
+      loading ||
+      passwordResetPending
+    )
+  }
+
   return (
     <>
       <Button
@@ -168,7 +178,7 @@ export const AuthenticationSettingsMenu = ({
             setUsername(e.target.value)
           }
           onKeyDown={async (e: React.KeyboardEvent<HTMLInputElement>) => {
-            if (e.key === "Enter" && username && password) {
+            if (e.key === "Enter" && isReadyForSubmission()) {
               await handleLogin()
             } else if (e.key === "Tab") {
               e?.stopPropagation()
@@ -194,7 +204,7 @@ export const AuthenticationSettingsMenu = ({
             setPassword(e.target.value)
           }
           onKeyDown={async (e: React.KeyboardEvent<HTMLInputElement>) => {
-            if (e.key === "Enter" && username && password) {
+            if (e.key === "Enter" && isReadyForSubmission()) {
               await handleLogin()
             }
             if (e.key === "Tab") {
@@ -232,12 +242,7 @@ export const AuthenticationSettingsMenu = ({
               e?.stopPropagation()
             }
           }}
-          disabled={
-            username === "" ||
-            password === "" ||
-            loading ||
-            passwordResetPending
-          }
+          disabled={!isReadyForSubmission()}
         >
           <Stack direction="row" alignItems="center">
             <Box width={24} height={24}>
@@ -267,7 +272,7 @@ export const AuthenticationSettingsMenu = ({
                 e?.stopPropagation()
               }
             }}
-            disabled={username === "" || loading || passwordResetPending}
+            disabled={!isReadyForSubmission()}
           >
             <Typography variant="body1">Reset Password</Typography>
           </Button>
